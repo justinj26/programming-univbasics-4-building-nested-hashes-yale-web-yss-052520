4b26d3569ae1bca65d2dd28f1d5c45751c67ea44
Justin James <justin.james@yale.edu>
2020-05-05 17:20:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,10 @@ def monopoly_with_second_tier
      :shortline_railroad => {}
    }
  }}
-
+ {:railroads => {
+   :pieces => 4,
+   :rent_in_dollars => {},
+   :names => {}
 end
 
 def monopoly_with_third_tier
